8eb0e28a9bde69ffef2889b4781c8fbc2f6a9552
Harry Pendleton <youngharry1997@gmail.com>
2019-07-16 16:57:39 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 
 
 
+
 //Putting first field into 'focus' state
 
 $("#name").focus();
@@ -31,14 +32,81 @@ $('#design').on('change',function(){
     $("option:contains('Dim Grey (I &#9829; JS shirt only)')").show();
 
   }
-  else if($('#design').val() === "js puns"){
-    $("option:contains('Tomato (I &#9829; JS shirt only)')").hide();
-    $("option:contains('Steel Blue (I &#9829; JS shirt only)')").hide();
-    $("option:contains('Dim Grey (I &#9829; JS shirt only)')").hide();
-    $("option:contains('Cornflower Blue (JS Puns shirt only)')").show();
-    $("option:contains('Dark Slate Grey (JS Puns shirt only)')").show();
-    $("option:contains('Gold (JS Puns shirt only)')").show();
+   if($('#design').val() === "js puns"){
+     $("option:contains('Cornflower Blue (JS Puns shirt only)')").show();
+     $("option:contains('Dark Slate Grey (JS Puns shirt only)')").show();
+     $("option:contains('Gold (JS Puns shirt only)')").show();
+
+    $("option:contains('Tomato')").hide();
+    $("option:contains('Steel Blue ')").hide();
+    $("option:contains('Dim Grey')").hide();
+
 }
+
+
+
+});
+
+let costOfActivity = 0;
+let anotherDollarSign = "$";
+
+let displayActivity = document.createElement('label');
+displayActivity.class = "activityCost";
+displayActivity.textContent = anotherDollarSign + costOfActivity;
+$('.activities').append(displayActivity);
+
+$('.activities').on('change',function(e){
+   let clickedActivities = $(e.target);
+    let clicked = clickedActivities.parent().text();
+    let $newLet = clicked.indexOf('$');
+  let newTotal =  clicked.slice($newLet + 1, $newLet.length);
+
+  newTotal = parseInt(newTotal);
+  if(clickedActivities.clicked = true){
+    costOfActivity = costOfActivity + newTotal;
+  }
+
+  if(clickedActivities.clicked = false){
+    costOfActivity = costOfActivity - newTotal;
+  }
+
+  //varaibles that you declare I think//
+  let dashDate = clicked.indexOf('-');
+  let comma = clicked.indexOf(',');
+// extracting the time and date from clickedActivities
+  let lastTotal = clicked.slice(comma, dashDate);
+
+  console.log(lastTotal);
+
+//Grabbing the checkboxes from input
+  const $newOne = $('[type="checkbox"]');
+
+// a for loop for each ofthe activity selected so that you can disable them
+  for(let i=0; $newOne.length; i++ ){
+    let $activityinteration = $newOne[i].parentNode;
+     let  activitycorrection = $activityinteration.parentElement.textContent;
+
+     if(activitycorrection.includes(Date)&& $activityinteration.textContent !== clickedActivities ){
+       $activityinteration.disabled = true;
+     }
+
+     if(clickedActivities.unchecked){
+        $activityinteration.disabled = false;
+
+     }
+};
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
